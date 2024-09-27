8ef807c77fa9e91624b3a7ea1571e79d97cb12c8
EmilAsk-Dev <carl.emil.ask1@gmail.com>
2024-09-27 15:35:49 +0200

    Uppgift 6

---
+++ Hemuppgift Enums & Properties/Uppgift 6 Skapa ett enum för användarroller/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_6_Skapa_ett_enum_för_användarroller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create different users with various roles
+            User admin = new User(UserRole.Admin);
+            User moderator = new User(UserRole.Moderator);
+            User user = new User(UserRole.User);
+            User guest = new User(UserRole.Guest);
+
+            // Print messages for each user
+            admin.PrintRoleMessage();     
+            moderator.PrintRoleMessage();  
+            user.PrintRoleMessage();       
+            guest.PrintRoleMessage();     
+
+            // Wait for user input before closing
+            Console.ReadKey();
+        }
+    }
+}
